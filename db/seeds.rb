d4db4e3aa661136a91a6d38e4c3b0bcf692a6f4e
Fanny Farrouch <fanny.farrouch@gmail.com>
2022-08-03 16:57:46 +0200

    seed done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Article.destroy_all
+
+puts "Creating articles..."
+article1 = {date: Date.today, title: "Coucou", content: "Coucou tout le monde!"}
+article2 =  {date: Date.today, title: "Salut", content: "Salut salut!!"}
+
+[article1, article2].each do |attributes|
+  article = Article.create!(attributes)
+  puts "Created #{article.title}"
+end
+puts "Finished!"
