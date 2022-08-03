acde0771ca339dfc5a21c48789c928b2970c5a22
Fanny Farrouch <fanny.farrouch@gmail.com>
2022-08-03 16:48:32 +0200

    Routes done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'articles#index'
+  root to: 'articles#index', as: :articles
+  # # get "articles", to: "articlex#index"
+  # get "articles/:id", to: "articles#show", as: :article
+  # get "articles/new", to: "articles#new"
+  # post "articles", to: "articles#create"
+
+  resources :articles, only: [:new, :create, :show]
 end
