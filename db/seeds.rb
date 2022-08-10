147879c5f509b9255d839781c6b0f92d01f34a34
Fanny Farrouch <fanny.farrouch@gmail.com>
2022-08-10 15:42:00 +0200

    Seed more accurate

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,8 @@ puts "Cleaning database..."
 Article.destroy_all
 
 puts "Creating articles..."
-article1 = {date: Date.today, title: "Coucou", content: "Coucou tout le monde!"}
-article2 =  {date: Date.today, title: "Salut", content: "Salut salut!!"}
+article1 = {date: Date.today, title: "Lorem ipsum dolor sit...", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pretium hendrerit enim. In nec metus elit. Aliquam neque lectus, molestie sed dui in, accumsan mollis orci. Sed scelerisque lobortis posuere. Sed nec nunc eget est ultrices egestas. Etiam porta facilisis rhoncus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam dignissim velit a molestie laoreet. Etiam elementum varius facilisis. Duis pharetra sodales varius. Suspendisse faucibus lectus eu ante porta ultricies. Morbi volutpat pharetra purus in ultricies."}
+article2 =  {date: Date.today, title: "Lorem ipsum dolor sit...", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pretium hendrerit enim. In nec metus elit. Aliquam neque lectus, molestie sed dui in, accumsan mollis orci. Sed scelerisque lobortis posuere. Sed nec nunc eget est ultrices egestas. Etiam porta facilisis rhoncus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nullam dignissim velit a molestie laoreet. Etiam elementum varius facilisis. Duis pharetra sodales varius. Suspendisse faucibus lectus eu ante porta ultricies. Morbi volutpat pharetra purus in ultricies."}
 
 [article1, article2].each do |attributes|
   article = Article.create!(attributes)
